86b15f144cfa5ec4f6d3f11fca4c5641baa44d86
suraj gupta <sandygupta0414@gmail.com>
2025-10-26 17:56:33 +0530

    ep03

--- App.js
+++ App.js
@@ -3,7 +3,21 @@
    
    // let heading = React.createElement('h1', {}, 'Hello, React!');
         const heading = React.createElement('h1', {id:'heading',xyz:'hello'}, 'Hello, React!');
+        console.log(heading);
+
+        const Heading = () => (
+         <>
+            <h1 id='heading' xyz='hello'>Hello, React from Component!</h1>
+            <h2>This is a functional component</h2>
+            </>
+        )
+
+        const jsxHeading = (<><h1 id='heading' xyz='hello'>Hello, React!</h1>{Heading()}</>)
+        console.log(jsxHeading);
+
+        
 
         let root = ReactDOM.createRoot(document.getElementById('root'));
 
-        root.render(heading);
+      //   root.render(heading);
+      root.render(jsxHeading);
